002b0426b0f362ca5fcd74380bdc7d6dba155d43
doriandel <doriandelorme@outlook.fr>
2023-01-30 00:44:29 +0100

    theme configuration udpate (will see if it works later)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 import '../styles/global.css';
-import { ChakraProvider } from '@chakra-ui/react';
-import { extendTheme } from '@chakra-ui/react';
-import { colors } from '../themes/foudations/colors';
+import { ChakraProvider, useTheme } from '@chakra-ui/react';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 
-const theme = extendTheme({ colors });
+const theme = useTheme();
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
